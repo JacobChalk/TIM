0fc5fe032d68b45c84e9f92acbbeb3d80b2466a5
Jacob Chalk <43956457+JacobChalk@users.noreply.github.com>
2024-05-12 07:54:04 +0100

    Removed unneeded functions

--- feature_extractors/omnivore/omnivore/datasets/perception_record.py
+++ feature_extractors/omnivore/omnivore/datasets/perception_record.py
@@ -1,16 +1,4 @@
 from .video_record import VideoRecord
-from datetime import timedelta
-import time
-
-
-def timestamp_to_sec(timestamp):
-    x = time.strptime(timestamp, '%H:%M:%S.%f')
-    sec = float(timedelta(hours=x.tm_hour,
-                          minutes=x.tm_min,
-                          seconds=x.tm_sec).total_seconds()) + float(
-                                  timestamp.split('.')[-1][:2]) / 100
-    return sec
-
 
 class PerceptionVideoRecord(VideoRecord):
     def __init__(self, tup):
